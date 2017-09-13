068d29f667f9cb65b569d54ab60e84162ad2f3e7
yaofong <yaofong@hotmail.com>
2017-09-13 22:48:23 +0800

    update to use Swift_Mime_SimpleMessage

--- src/ElasticTransport.php
+++ src/ElasticTransport.php
@@ -4,12 +4,12 @@ namespace ArtisanMY\LaravelElasticEmail;
 
 use GuzzleHttp\ClientInterface;
 use Illuminate\Mail\Transport\Transport;
-use Swift_Mime_Message;
+use Swift_Mime_SimpleMessage;
 
 class ElasticTransport extends Transport
 {
-	
-	/**
+    
+    /**
      * Guzzle client instance.
      *
      * @var \GuzzleHttp\ClientInterface
@@ -43,12 +43,12 @@ class ElasticTransport extends Transport
      * @param  \GuzzleHttp\ClientInterface  $client
      * @param  string  $key
      * @param  string  $username
-	 *
+     *
      * @return void
      */
     public function __construct(ClientInterface $client, $key, $account)
     {
-    	$this->client = $client;
+        $this->client = $client;
         $this->key = $key;
         $this->account = $account;
     }
@@ -56,7 +56,7 @@ class ElasticTransport extends Transport
     /**
      * {@inheritdoc}
      */
-    public function send(Swift_Mime_Message $message, &$failedRecipients = null)
+    public function send(Swift_Mime_SimpleMessage $message, &$failedRecipients = null)
     {
         $this->beforeSendPerformed($message);
        
@@ -68,17 +68,17 @@ class ElasticTransport extends Transport
             'msgBcc' => $this->getEmailAddresses($message, 'getBcc'),
             'msgFrom' => $this->getFromAddress($message)['email'],
             'msgFromName' => $this->getFromAddress($message)['name'],
-			'from' => $this->getFromAddress($message)['email'],
-			'fromName' => $this->getFromAddress($message)['name'],
-			'to' => $this->getEmailAddresses($message),
+            'from' => $this->getFromAddress($message)['email'],
+            'fromName' => $this->getFromAddress($message)['name'],
+            'to' => $this->getEmailAddresses($message),
             'subject' => $message->getSubject(),
             'body_html' => $message->getBody(),
-	    'body_text' => $this->getText($message)
+        'body_text' => $this->getText($message)
         ];
 
         $result = $this->client->post($this->url, [
-        	'form_params' => $data
-		]);
+            'form_params' => $data
+        ]);
         
         return $result;
     }
@@ -89,42 +89,42 @@ class ElasticTransport extends Transport
      * @param  \Swift_Mime_Message $message
      * @return text|null
      */
-    protected function getText(Swift_Mime_Message $message)
+    protected function getText(Swift_Mime_SimpleMessage $message)
     {
         $text = null;
         
         foreach($message->getChildren() as $child)
-		{
-			if($child->getContentType() == 'text/plain')
-			{
-				$text = $child->getBody();
-			}
-		}
+        {
+            if($child->getContentType() == 'text/plain')
+            {
+                $text = $child->getBody();
+            }
+        }
         
         return $text;
     }
-	
-	/**
-	 * @param \Swift_Mime_Message $message
-	 *
-	 * @return array
-	 */
-    protected function getFromAddress(Swift_Mime_Message $message)
-	{
-		return [
-			'email' => array_keys($message->getFrom())[0],
-			'name' => array_values($message->getFrom())[0],
-		];
-	}
-	
-	protected function getEmailAddresses(Swift_Mime_Message $message, $method = 'getTo')
-	{
-		$data = call_user_func([$message, $method]);
-		
-		if(is_array($data))
-		{
-			return implode(',', array_keys($data));
-		}
-		return '';
-	}
+    
+    /**
+     * @param \Swift_Mime_Message $message
+     *
+     * @return array
+     */
+    protected function getFromAddress(Swift_Mime_SimpleMessage $message)
+    {
+        return [
+            'email' => array_keys($message->getFrom())[0],
+            'name' => array_values($message->getFrom())[0],
+        ];
+    }
+    
+    protected function getEmailAddresses(Swift_Mime_SimpleMessage $message, $method = 'getTo')
+    {
+        $data = call_user_func([$message, $method]);
+        
+        if(is_array($data))
+        {
+            return implode(',', array_keys($data));
+        }
+        return '';
+    }
 }
